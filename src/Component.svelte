2f0ad4142eaf4772e655461a487fe36b50858493
poirazis <poirazis@gmail.com>
2022-12-03 12:57:11 +0200

    Beta Version

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,5 +1,4 @@
 <script>
-  import "@spectrum-css/treeview/dist/index-vars.css"
   import TreeItem from "./TreeItem.svelte";
   import { getContext } from "svelte"
 
@@ -11,10 +10,16 @@
   export let standalone = true
   export let width = "250px"
   export let dataProvider 
-  export let showColumn = "_id"
-  export let childColumn = ""
-  export let icon
+  export let parentValue
+  export let parentID
+  export let parentIcon
+  export let parentOnClick
+  export let childColumn
+  export let childIcon
+  export let childOnClick
   export let size
+
+  let linkTable
 
     // If the parent DataProvider is loading, fill the rows array with a number of empty objects 
     // corresponding to the DataProvider's page size; 
@@ -22,26 +27,50 @@
   $: rows = $loading
     ? new Array(dataProvider?.limit > 20 ? 20 : dataProvider?.limit).fill({})
     : dataProvider?.rows
-</script>
 
+  $: linkTable = dataProvider?.schema[childColumn]?.type === "link"
+</script>
 <div use:styleable={$component.styles}>
+    {#if !linkTable}
+      <span class="error"> Child Column not a Relationship </span>
+    {/if}
+
     <ul
       class:spectrum-TreeView--standalone={standalone}
       class:spectrum-TreeView--quiet={quiet}
       class="spectrum-TreeView spectrum-Treeview--size{size}"
       style="width: {width}"
     >
+    
     {#if rows?.length > 0}
       {#each rows as row }
-        <TreeItem title={row[showColumn]} {icon} {size}>
-        {#if row[childColumn] && Array.isArray(row[childColumn])}
-          {#each row[childColumn] as child}
-            <TreeItem title={child.primaryDisplay} {icon} {size} >  </TreeItem>
-          {/each}
-        {/if}
+        <TreeItem 
+          id={row[parentID]} 
+          title={row[parentValue]} 
+          icon={parentIcon} {size} 
+          onClick={parentOnClick}
+          >
+          {#if row[childColumn] && Array.isArray(row[childColumn])}
+            {#each row[childColumn] as child}
+              <TreeItem 
+                id={child._id} 
+                title={child.primaryDisplay}
+                onClick={childOnClick}
+                icon={childIcon} {size} >
+              </TreeItem>
+            {/each}
+          {/if}
         </TreeItem>
       {/each}
-      {/if}
+    {/if}
       <slot />
   </ul>
 </div>
+
+<style>
+  .error {
+    border: 2px dotted red;
+    margin: 1rem;
+    padding: 0.5rem 1rem;
+  }
+</style>
